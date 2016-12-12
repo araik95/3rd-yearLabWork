e3e5688b57b408ae061d1c6e1d7fef5e13ef7059
Araik Martirosyan <martirosyanara77@gmail.com>
2016-12-13 00:56:45 +0400

    Last Update For Labwork Part Two

--- ANMT Labs/ANMT Part Lab Two/LabTwo/LabTwo/Program.cs
+++ ANMT Labs/ANMT Part Lab Two/LabTwo/LabTwo/Program.cs
@@ -18,10 +18,17 @@ namespace LabTwo
 
         static void Main(string[] args)
         {
-            //simpleIterationMethod_ForTwoUnknown();
-            //simpleIterationMethod_V1(); 
-            //nyutonMethod_V1(); 
-            nyutonMethod_ForTwoUnknown();
+
+             simpleIterationMethod_ForOneUnknown();
+             simpleIterationMethod_ForTwoUnknown();
+
+             nyutonMethod_ForOneUnknown();
+             nyutonMethod_ForTwoUnknown();
+
+            diff_One();
+            diff_Two();
+
+            Console.ReadLine();
         }
 
         /*   example for 
@@ -37,10 +44,7 @@ namespace LabTwo
             {           
                 count++;
                 x_new = (1 - Math.Log10(2 * x_old + 3)) / 2;
-                Console.WriteLine("Iteration number = " + count);
-                Console.WriteLine("X_New = {0:0.0000}",x_new);
-                Console.WriteLine("F(x) = {0:0.0000}", 2 * x_new + Math.Log10(2 * x_new + 3) - 1);
-                Console.WriteLine();
+                consoleWriteSecondFunction(x_new, count);
 
                 if (Math.Abs(x_old - x_new) < 0.000001 || count == 1000) break;
                 x_old = x_new;
@@ -65,12 +69,7 @@ namespace LabTwo
                 x_new = (Math.Cos(y_old) / 3) + 0.3;
                 y_new = Math.Sin(x_old - 0.6) - 1.6;
 
-                Console.WriteLine("Iteration number = " + count);
-                Console.WriteLine("X_New = {0:0.0000}", x_new);
-                Console.WriteLine("Y_New = {0:0.0000}", y_new);
-                Console.WriteLine("F1(X_New,Y_New) = {0:0.0000}", 3 * x_new - Math.Cos(y_new) - 0.9 );
-                Console.WriteLine("F2(X_New,Y_New) = {0:0.0000}", Math.Sin( x_new - 0.6) - y_new - 1.6);
-                Console.WriteLine();
+                consoleWrite(x_new, y_new, count);
 
                 if (Math.Abs(x_old - x_new) < 0.000001 || count == 1000) break;
                 x_old = x_new;
@@ -90,22 +89,15 @@ namespace LabTwo
             x_old = x0;
             count = 0;
 
-            Console.WriteLine("Iteration number = " + count);
-            Console.WriteLine("X_New = {0:0.0000}", x_old);
-            Console.WriteLine("F(x) = {0:0.0000}", 2 * x_old + Math.Log10(2 * x_old + 3) - 1);
-            Console.WriteLine();
+            consoleWriteSecondFunction(x_old, count);
 
             while (true)
             {
                 count++;
                 x_new = x_old - (2 * x_old + Math.Log10(2 * x_old + 3) - 1) 
-                    / (2 / (Math.Log(10) * (2 * x0 + 3)) + 2);
+                    / (2 / (Math.Log(10) * (2 * x_old + 3)) + 2);
 
-                Console.WriteLine("Iteration number = " + count);
-                Console.WriteLine("X_New = {0:0.0000000}", x_new);
-                Console.WriteLine("F(x) = {0:0.0000}",  2 * x_new + Math.Log10(2 * x_new + 3) - 1 );
-                Console.WriteLine();
-
+                consoleWriteSecondFunction(x_new, count);
                 if (Math.Abs(x_old - x_new) < 0.000001 || count == 1000) break;
                 x_old = x_new;
             }
@@ -131,11 +123,11 @@ namespace LabTwo
                 {Math.Cos(x_old - 0.6 ), -1 }
             });
             Vector<double> vectorOf_f1_f2 = vector_f1_f2(x_old, y_old);
-            Vector<double> matrix_old = DenseVector.OfArray(new double[]
+            Vector<double> vector_old = DenseVector.OfArray(new double[]
             {
                 x_old,y_old
             });
-            Vector<double> matrix_new;
+            Vector<double> vector_new;
 
             Console.WriteLine(matrixOf_Diff_F1_F2);
             Console.WriteLine(vectorOf_f1_f2);
@@ -143,17 +135,12 @@ namespace LabTwo
             while (true)
             {
                     count++;
-                    matrix_new = matrix_old - ( matrixOf_Diff_F1_F2.Inverse() * vector_f1_f2(matrix_old[0],matrix_old[1]) ); 
+                    vector_new = vector_old - ( matrixOf_Diff_F1_F2.Inverse() * vector_f1_f2(vector_old[0],vector_old[1]) );
 
-                    Console.WriteLine("Iteration number = " + count);
-                    Console.WriteLine("X_New = {0:0.0000} \nY_New = {1:0.0000}", matrix_new[0],matrix_new[1]);
+                    consoleWrite(vector_new[0], vector_new[1], count);
 
-                    Console.WriteLine("F1(X_New,Y_New) = {0:0.0000}", 3 * matrix_new[0] - Math.Cos(matrix_new[1]) - 0.9);
-                    Console.WriteLine("F2(X_New,Y_New) = {0:0.0000}", Math.Sin(matrix_new[0] - 0.6) - matrix_new[1] - 1.6);
-                    Console.WriteLine();
-
-                    if (Math.Abs(matrix_old.Sum() - matrix_new.Sum()) < 0.000001 || count == 1000) break;
-                    matrix_old = matrix_new;
+                if (Math.Abs(vector_old.Sum() - vector_new.Sum()) < 0.000001 || count == 1000) break;
+                    vector_old = vector_new;
             }
         }
 
@@ -162,9 +149,121 @@ namespace LabTwo
             Vector<double> vectorOf_f1_f2 = DenseVector.OfArray(new double[]
             {
                 3 * x - Math.Cos(y) - 0.9,
-                Math.Sin( x -0.6) - y - 1.6
+                Math.Sin( x - 0.6) - y - 1.6
             });
             return vectorOf_f1_f2;
+        }
+
+        /*  example for 
+        *              f1 = 3 * x - cos y - 0.9 = 0
+        *              f2 = sin( x - 0.6 ) - y - 1.6 = 0 
+        *              Starting point (0;-1)   
+        *              K1 = 1 K2 = 0.5 H = 0.01     
+       */
+
+        static void diff_One()
+        {
+            Console.WriteLine("**************** Diff One Method ****************\n\n");
+            count = 0;
+            x_old = 0;
+            y_old = -1;
+            double k1 = 1;
+            double k2 = 0.5;
+            double h = 0.01;
+            Vector<double> vector_old = DenseVector.OfArray(new double[]
+            {
+                x_old,y_old
+            });
+            Vector<double> vector_new;
+
+            while (true)
+            {
+                count++;
+                vector_new = vector_old - h * vectorOf_Nabla_f1_f2(vector_old[0], vector_old[1]);
+
+                if ( count % 100 == 0) 
+                    consoleWrite(vector_new[0], vector_new[1], count);
+                
+
+                if (Math.Abs(vector_old.Sum() - vector_new.Sum()) < 0.000001 || count == 10000)
+                {
+                    consoleWrite(vector_new[0], vector_new[1], count);
+                    break;
+                }
+                vector_old = vector_new;
+            }
+        }
+
+        /*  example for 
+         *         f1 = 3 * x - cos y - 0.9 = 0
+         *         f2 = sin( x - 0.6 ) - y - 1.6 = 0 
+         *         Starting point (0;-1)   
+         *         K1 = 1 K2 = 0.5 H = 0.01  Gamma = 1  
+        */
+        static void diff_Two()
+        {
+            Console.WriteLine("**************** Diff Two Method ****************\n\n");
+            count = 0;
+            x_old = 0;
+            y_old = -1;
+            double k1 = 1;
+            double k2 = 0.5;
+            double h = 0.01;
+            Vector<double> vector_old = DenseVector.OfArray(new double[]
+            {
+                x_old,y_old
+            });
+            Vector<double> vector_new;
+            Vector<double> vector_old2 = DenseVector.OfArray(new double[]
+           {
+                x_old,y_old
+           });
+            double gamma = 1;
+
+            vector_new = (1 / (2 + gamma * h)) * (4 * vector_old - 2 * Math.Pow(h, 2) * vectorOf_Nabla_f1_f2(vector_old[0], vector_old[1]));
+            Console.WriteLine(vector_new);
+            while (true)
+            {
+                count++;
+                vector_new = (1 / (2 + gamma * h)) * (4 * vector_old - (2 - gamma * h) * vector_old2 - 2 * Math.Pow(h, 2) * vectorOf_Nabla_f1_f2(vector_old[0], vector_old[1]));
+                vector_old2 = vector_old;
+
+                if (count % 100 == 0)
+                    consoleWrite(vector_new[0], vector_new[1], count);
+                                
+                if (Math.Abs(vector_old.Sum() - vector_new.Sum()) < 0.000001 || count == 10000){
+                    consoleWrite(vector_new[0], vector_new[1], count);
+                    break;
+                }
+                vector_old = vector_new;
+            }
+        }
+
+        static Vector<double> vectorOf_Nabla_f1_f2(double x, double y)
+        {
+           Vector<double> vectorOf_Nabla_f1_f2 = DenseVector.OfArray(new double[]
+           {
+                18 * x - 6 * Math.Cos(y) - Math.Cos(x - 0.6) * (y - Math.Sin(x - 0.6) + 1.6) - 5.4,
+                y - Math.Sin(x - 0.6) - 2 * Math.Sin(y) * (Math.Cos(y) - 3 * x + 0.9) + 1.6
+           });
+           return vectorOf_Nabla_f1_f2;
+        }
+
+        static void consoleWrite(double x1,double x2,int count)
+        {
+            Console.WriteLine("Iteration number = " + count);
+            Console.WriteLine("X_New = {0:0.000000} \nY_New = {1:0.000000}", x1, x2);
+            Console.WriteLine("F1(X_New,Y_New) = {0:0.000000}", 3 * x1 - Math.Cos(x2) - 0.9);
+            Console.WriteLine("F2(X_New,Y_New) = {0:0.000000}", Math.Sin(x1 - 0.6) - x2 - 1.6);
+            Console.WriteLine();
+        }
+
+        static void consoleWriteSecondFunction(double x1,int count)
+        {
+            Console.WriteLine("Iteration number = " + count);
+            Console.WriteLine("X_New = {0:0.0000000}", x1);
+            Console.WriteLine("F(x) = {0:0.0000000}", 2 * x1 + Math.Log10(2 * x1 + 3) - 1);
+            Console.WriteLine();
         }
     }
 }
